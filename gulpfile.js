ebf6c85f3bb3227ba2fb26c2f2abc59349367adc
Jurie-Jan Botha <juriejanbotha@gmail.com>
2016-09-19 18:27:20 +0200

    Enabled livereload

--- gulpfile.js
+++ gulpfile.js
@@ -7,46 +7,57 @@ const liveServer = require('gulp-live-server')
 const markdown = require('gulp-markdown')
 const markdownPdf = require('gulp-markdown-pdf')
 const rename = require('gulp-rename')
+const wrap = require('gulp-wrap')
 
-gulp.task('cleanImages', function () {
-  return gulp.src('./images/**/*.png')
-    .pipe(clean())
-})
+const paths = {
+  content: './content.md',
+  images: './images',
+  www: './www'
+}
 
-gulp.task('clean', function () {
-  return gulp.src('./www')
-    .pipe(clean())
-})
+function cleanImages () {
+  return gulp.src(`${paths.images}/**/*.png`).pipe(clean())
+}
 
-gulp.task('formatImages', function () {
-  return gulp.src('./images/**/*.png')
+function formatImages () {
+  return gulp.src(`${paths.images}/**/*.png`)
     .pipe(jimp({'': {type: 'jpg', quality: 80}}))
     .pipe(gulp.dest('./images'))
-})
+}
 
-gulp.task('copyImages', function () {
-  return gulp.src('./images/**/*')
-    .pipe(gulp.dest('./www/images'))
-})
+function cleanRoot () {
+  return gulp.src(paths.www).pipe(clean())
+}
+
+function linkImages () {
+  return gulp.src(`${paths.images}/**/*`)
+    .pipe(gulp.symlink(`${paths.www}/images`))
+}
+
+function html () {
+  return gulp.src(paths.content)
+    .pipe(markdown())
+    .pipe(rename('./index.html'))
+    .pipe(wrap('<html><head></head><body><%= contents %></body></head>'))
+    .pipe(gulp.dest(paths.www))
+}
+
+function serve (done) {
+  let server = liveServer.static(paths.www)
+  server.start()
+  gulp.watch(paths.content, html)
+  gulp.watch(paths.www).on('change', (path) => {
+    server.notify({type: 'change', path})
+  })
+  done()
+}
 
 gulp.task('pdf', function () {
-  return gulp.src('./content.md')
+  return gulp.src(paths.content)
     .pipe(markdownPdf({cssPath: './print.css'}))
     .pipe(gulp.dest('.'))
 })
 
-gulp.task('html', function () {
-  return gulp.src('./content.md')
-    .pipe(markdown())
-    .pipe(rename('./index.html'))
-    .pipe(gulp.dest('./www'))
-})
-
-gulp.task('serve', function (done) {
-  let server = liveServer.static('./www')
-  server.start()
-  done()
-})
-
-gulp.task('build', gulp.series('html', 'copyImages'))
-gulp.task('default', gulp.series('clean', 'build', 'serve'))
+gulp.task('format', gulp.series(formatImages, cleanImages))
+gulp.task('build', gulp.series(html, linkImages))
+gulp.task('default', gulp.series(cleanRoot, 'build', serve))
